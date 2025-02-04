7c41b2846b44d3bf9e58e2da99e7cd91596d652a
Zaker Safdari <zakersafdari@gmail.com>
2025-02-05 12:42:00 +1300

    Add: Create foundation-models.py file for foundational model implementations

---
+++ foundation-models/foundation-models.py
@@ -0,0 +1,23 @@
+# What are Foundation Models?
+# Foundation models are large-scale pre-trained models that can be fine-tuned for different tasks like image recognition, text generation and multimodal Ai. 
+# Multi-modal AI is a type of artificial intelligence that can process multiple types of data, such as images, speech, and text, in a single model.
+# Foundation models are trained on large-scale datasets and can be fine-tuned for a variety of tasks.
+# Examples of Foundation Models:
+# 1. OpenAI's GPT-4 (Text generation, chatbots)
+# 2. BLIP (Vision-language models like captioning, VQA)
+# 3. CLIP (Image & text understanding)
+# 4. DALL-E (AI-generated images)
+# 5. Gemini AI (Google's multimodal model)
+# 6. Flamingo (Facebook's multimodal model for vision and text generation)
+
+# These models are pre-trained on massive datasets and can be adapted to different AI applications.
+
+
+# How do Foundation Models work?
+# 1. Pre-training: Them model is trained on huge datasets(text, images, audio etc). This step is computationally expensive and requires large amounts of data.
+# 2. Fine-tuning: The model is adapted for specific tasks (e.g. medical AI, finance, robotics) using smaller datasets. This step is less computationally expensive.
+# 3. Inference: The model is used to make predictions/ generate results based on new data/input. This step is fast and efficient.
+# Example:
+# GPT-4 writes essays, answers questions.
+# BLIP generates image captions, answers visual questions.
+# CLIP matches images with text descriptions.  
