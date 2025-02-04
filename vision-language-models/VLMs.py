e45306d10bd0e04c68324df58ab96268930ccf0b
Zaker Safdari <zakersafdari@gmail.com>
2025-02-05 11:24:54 +1300

    Add: Implemented Vision-Language Models tutorial with image captioning example and necessary libraries

---
+++ vision-language-models/VLMs.py
@@ -0,0 +1,51 @@
+# What are Vision-Language Models?
+# Vision-Language Models (VLMs) are AI models that can process both images and text together to perform tasks like: 
+# Image Captioning which is generating descriptions for iamges.
+# Visual Question Answering (VQA) which is answering questions about images.
+# Image-Text Retrieval which is finding images based on text queries and vice versa.
+# Multimodal Reasoning which is understanding relationships between visual and textual content.
+
+# Examles of VLMS:
+# 1. CLIP (OpenAI) which matches images with text descriptions.
+# 2. BLIP (Salesforce) used for image captioning and VQA.
+# 3. LLaVA (Large Language and Vision Assistant) which is GPT-4-like model for images.
+
+
+# Step 1: Install Pre-Trained Vision-Language Models
+
+# For first hands-on experiment, install Hugging Face Transformers library which provides pre-trained VLMs and Pillow library for image processing.
+
+# pip install transformers torch torchvision pillow
+# we will use BLIP(Bootstrapped Language-Image Pre-training) model for image captioning.
+
+
+# Step 2: Image Captioning with BLIP
+# build a simple program that takes an image and generates a caption.
+# an image was saved in the directory
+
+# Step 3: Write the code
+
+# import libraries
+from transformers import BlipProcessor, BlipForConditionalGeneration
+from PIL import Image
+import torch
+
+# Load the BlIP processor and model
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
+
+# Load the image
+image_path = "image1.jpg" 
+image = Image.open(image_path).convert("RGB")
+
+# Preprocess the image and generate a caption 
+inputs = processor(images=image, return_tensors="pt") # preprocess the image using the processor and convert it to PyTorch tensors. PyTorch tensors are used as input to the model. PyTorch is a popular deep learning library.
+with torch.no_grad(): # here we are using torch.no_grad() to disable gradient calculation which is not needed for inference. gradient calculation is used for training. 
+    caption = model.generate(**inputs) # generate the caption using the model and inputs generated from the image.
+    
+# Decode and print the caption
+generated_caption = processor.decode(caption[0], skip_special_tokens=True)
+print("Generated Caption:", generated_caption)
+
+# Step 4: Run the code
+# run the code and it will generate a caption for the image.
